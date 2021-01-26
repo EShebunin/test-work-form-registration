3e610decf7ddf37621c4f404ebac54b740b7386a
Evgeny Shebunin <evgen.shebunin@gmail.com>
2021-01-26 16:37:41 +0300

    feat: add btn component

---
+++ src/ui-components/components/Button/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button :disabled="disabled" class="ui-button">
+    <slot />
+  </button>
+</template>
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
